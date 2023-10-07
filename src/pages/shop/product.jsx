98591dc79c58ee2f0b2af54e32be1619f76ead44
krishn8581 <111411969+krishn8581@users.noreply.github.com>
2023-10-07 19:00:13 +0530

    first commit

---
+++ src/pages/shop/product.jsx
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+
+export const Product = (props) => {
+  const { id, productName, price, productImage, title, description, category } = props.data;
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  const cartItemCount = cartItems[id];
+
+  return (
+    <div className="product">
+      <img src={productImage} />
+      <div className="description">
+        <p>
+          <b>{title}</b>
+        </p>
+        <p> <b>{price}</b></p>
+        <p> {description}</p>
+        <p> {category}</p>
+      </div>
+      <button className="addToCartBttn" onClick={() => addToCart(id)}>
+        Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
+      </button>
+    </div>
+  );
+};
---
+++ src/products.js
@@ -0,0 +1,58 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price:  "Price: 109.95",
+    description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    productImage: product1,
+  },
+  {
+    id: 2,
+    title: "Mens Casual Premium Slim Fit T-Shirts",
+    price:  "Price: 22.3",
+    description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+    category: "men's clothing",
+    productImage: product2,
+  },
+  {
+    id: 3,
+    title: "Mens Cotton Jacket",
+    price:  "Price: 55.99",
+    description: "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+    category: "men's clothing",
+    productImage: product3,
+  },
+  {
+    id: 4,
+    title: "Mens Casual Slim Fit",
+    price:  "Price: 15.99",
+    description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    productImage: product4,
+  },
+  {
+    id: 5,
+    title: "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+    price:  "Price: 695",
+    description: "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+    category: "jewelery",
+    productImage: product5,
+  },
+  {
+    id: 6,
+    title: "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+    price:  "Price: 168",
+    description: "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+    category: "jewelery",
+    productImage: product6,
+  },
+
+];
